1fddc4e5b91c7029b536720828ad6107f56b71f2
Mansik18 <taktashev.m@gmail.com>
2021-01-27 16:36:28 +0600

    Condition

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Mansur<nav class="user-menu">'; // укажите здесь ваше имя
+$user_name = 'Mansur'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
