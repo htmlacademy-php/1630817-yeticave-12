ffb0d5845b605b67888cc04f523b1347d43d534c
Mansik18 <taktashev.m@gmail.com>
2021-02-10 20:04:36 +0600

    debug

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ require_once 'dbConfig.php';
 require_once 'sql_requests.php';
 
 $is_auth = rand(0, 1);
+$categories = get_categories($con);
+$lots = get_lots($con);
 
-$page_content = include_template('main.php', ['categories' => get_categories($con), 'lots' => get_lots($con) ]);
-print (include_template('layout.php', ['categories' => get_categories($con), 'is_auth' =>$is_auth, 'main_content' =>$page_content, 'user_name' => 'Mansur', 'title' => 'Главная']));
+$page_content = include_template('main.php', ['categories' => $categories, 'lots' => $lots ]);
+print (include_template('layout.php', ['categories' => $categories, 'is_auth' =>$is_auth, 'main_content' =>$page_content, 'user_name' => 'Mansur', 'title' => 'Главная']));
 
--- lot.php
+++ lot.php
@@ -6,18 +6,19 @@ require_once 'dbConfig.php';
 require_once 'sql_requests.php';
 
 $is_auth = rand(0, 1);
+$categories = get_categories($con);
 
 if(isset($_GET['lot_id']) && !empty($_GET['lot_id']) && is_numeric($_GET['lot_id'])){
     $lot  = get_lot_info($con,$_GET['lot_id']);
     if(!empty($lot)){
         $bets = get_lot_bets($con,$_GET['lot_id']);
-        $page_content = include_template('lot.php', ['categories' => get_categories($con), 'lot' => $lot[0], 'bets' =>$bets]);
-        print (include_template('layout.php', ['categories' => get_categories($con),'user_name' => 'Mansur', 'is_auth' =>$is_auth,'title' => 'Лот', 'main_content' =>$page_content]));
+        $page_content = include_template('lot.php', ['categories' => $categories, 'lot' => $lot[0], 'bets' =>$bets]);
+        print (include_template('layout.php', ['categories' => $categories, 'user_name' => 'Mansur', 'is_auth' => $is_auth,'title' => 'Лот', 'main_content' =>$page_content]));
     }
     else{
-        print (include_template('layout.php', ['categories' => get_categories($con),'user_name' => 'Mansur', 'is_auth' =>$is_auth,'title' => 'Ошибка 404', 'main_content' => include_template('404.php',['categories' =>get_categories($con)])]));
+        print (include_template('layout.php', ['categories' => $categories,'user_name' => 'Mansur', 'is_auth' => $is_auth,'title' => 'Ошибка 404', 'main_content' => include_template('404.php',['categories' => $categories])]));
     }
 }
 else{
-    print (include_template('layout.php', ['categories' => get_categories($con),'user_name' => 'Mansur', 'is_auth' =>$is_auth,'title' => 'Ошибка 404', 'main_content' =>include_template('404.php',['categories' =>get_categories($con)])]));
+    print (include_template('layout.php', ['categories' => $categories,'user_name' => 'Mansur', 'is_auth' => $is_auth,'title' => 'Ошибка 404', 'main_content' =>include_template('404.php',['categories' => $categories])]));
 }
